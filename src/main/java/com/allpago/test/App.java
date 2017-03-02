4d1393d331f2684624c791af0ef8e51ce93ff05e
Chaklader Asfak Arefe <omi.chaklader@gmail.com>
2017-03-02 10:07:22 +0600

    setting up the project

---
+++ src/main/java/com/allpago/test/App.java
@@ -0,0 +1,12 @@
+package com.allpago.test;
+
+/**
+ * Created by Chaklader on 3/2/17.
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, let's rool on!");
+    }
+}
